64d396ed45619c6d513d7b5741f76acc1825c72c
yurifuko <yurifuko@proton.me>
2023-01-20 18:06:24 +0100

    eadsdsa

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,4 +1,4 @@
-var r_text = ["Religion is retarded!", "Always give up.", "Sorry to keep you waiting.", "What's good, melancholy?", "A lie told often becomes the truth.", "I do not want to fit in.", "All hope is gone.", "If your first attempt doesn't work, give up.", "It's only going to get worse.", "SMEG-HEAD", "Cha cha cha.", "Kapow!", "The inner machinations of my mind are an enigma.", "Where's yo head at?!", "ᗜˬᗜ 🖕", "(ó﹏ò｡)", "╮(￣ω￣;)╭"];
+var r_text = ["Religion is retarded!", "Always give up.", "Sorry to keep you waiting.", "What's good, melancholy?", "A lie told often becomes the truth.", "I do not want to fit in.", "All hope is gone.", "If your first attempt doesn't work, give up.", "It's only going to get worse.", "SMEG-HEAD", "Cha cha cha.", "Kapow!", "The inner machinations of my mind are an enigma.", "Where's yo head at?!", "ᗜˬᗜ 🖕", "Pain is due.", "It doesn't matter after all.", "What are you waiting for?"];
 
 var i = Math.floor(r_text.length * Math.random()); 
 
