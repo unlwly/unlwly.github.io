b19ff107b506e6ce8d906bb0577a1bbb12d7cd96
unlwly <unlwly@proton.me>
2023-03-01 18:13:19 +0100

    nb

---
+++ COPYING.md
@@ -0,0 +1,7 @@
+# licensing
+
+- original code is provided under BSD3clause
+- [Iosevka](https://typeof.net/Iosevka/) &middot; available under SIL OFL
+- [Bootstrap Icons](https://icons.getbootstrap.com/) &middot; licensed under MIT
+- [Simple Icons](https://simpleicons.org) &middot; licensed under CC0
+- [typed.js](https://github.com/mattboldt/typed.js/) &middot; licensed under MIT
--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,5 +1,11 @@
-var r_text = ["Religion is retarded!", "Always give up.", "Sorry to keep you waiting.", "What's good, melancholy?", "A lie told often becomes the truth.", "I want to fit in.", "All hope is gone.", "If your first attempt doesn't work, give up.", "It's only going to get worse.", "SMEG-HEAD", "The inner machinations of my mind are an enigma.", "Where's yo head at?!", "ᗜˬᗜ 🖕", "Pain is due.", "It doesn't matter after all.", "What are you waiting for?"];
-
+var r_text = [
+    "Always give up.", 
+    "All hope is gone.", 
+    "If your first attempt doesn't work, give up.", 
+    "It's only going to get worse.", 
+    "The inner machinations of my mind are an enigma.", 
+    "Pain is due.", 
+    "It doesn't matter after all."
+];
 var i = Math.floor(r_text.length * Math.random()); 
-
 document.write("<q>" + r_text[i]  + "</q>");
